00790a3f234fb946274f5de0669849f5d9b92a7a
estiven2111 <estiven.a.martinez@gmail.com>
2024-10-26 18:30:54 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("CLASE INGENIERIA DEL SOFTWARE");
+})
+module.exports = app 
